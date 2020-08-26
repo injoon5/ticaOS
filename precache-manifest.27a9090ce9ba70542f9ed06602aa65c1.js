d296c6fb9607045301b877b37bf949abe5746072
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-26 04:51:44 +0000

    Deploy os.blokode.xyz to github.com/thoratica/ticaOS.git:gh-pages

---
+++ precache-manifest.27a9090ce9ba70542f9ed06602aa65c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a9b60885b4546278da521cc612feba7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97c50142c879efeb90eb",
+    "url": "/static/css/main.3b547580.chunk.css"
+  },
+  {
+    "revision": "9d1ccc48d3ba9061ecf3",
+    "url": "/static/js/2.037b5cfd.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.037b5cfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97c50142c879efeb90eb",
+    "url": "/static/js/main.347c985b.chunk.js"
+  },
+  {
+    "revision": "7aa5849725676729a38c",
+    "url": "/static/js/runtime-main.2b0e1c4f.js"
+  }
+]);
